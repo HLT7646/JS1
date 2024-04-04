313a915f4f8dfdb7d4bb9b595d44482e692eccca
HLT7646 <hlthlt1705@gmail.com>
2024-04-04 13:56:13 +0700

    Ha

--- Number/ex1.js
+++ Number/ex1.js
@@ -1,6 +1,9 @@
 function calculateSphereVolume(radius) {
-    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-    return volume;
+  if (radius <= 0) {
+    return 0;
   }
-  const radius = 5;
-  console.log(calculateSphereVolume(radius));
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  return volume;
+}
+const radius = 5;
+console.log(calculateSphereVolume(radius));
